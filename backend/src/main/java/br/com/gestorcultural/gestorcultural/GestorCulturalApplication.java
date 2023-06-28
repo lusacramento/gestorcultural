bbf57b6a424b1acf649768724a56dd9b90387d87
lusacramento <lucianossacramento@yahoo.com.br>
2023-06-28 13:22:47 -0300

    init springboot

---
+++ backend/src/main/java/br/com/gestorcultural/gestorcultural/GestorCulturalApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestorcultural.gestorcultural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorCulturalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorCulturalApplication.class, args);
+	}
+
+}

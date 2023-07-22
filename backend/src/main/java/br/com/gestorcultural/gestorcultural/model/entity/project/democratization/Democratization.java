9b151a3d96ad7304d0431a92cd79031ea1872708
lusacramento <lucianossacramento@yahoo.com.br>
2023-07-22 18:19:52 -0300

    implement democratization entity

---
+++ backend/src/main/java/br/com/gestorcultural/gestorcultural/model/entity/project/democratization/Democratization.java
@@ -0,0 +1,11 @@
+package br.com.gestorcultural.gestorcultural.model.entity.project.democratization;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
+public class Democratization {
+    @Id
+    private String id;
+    private String democratization;
+}

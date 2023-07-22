3269cb18b57b5577edfc3fd2fa539861ebccc555
lusacramento <lucianossacramento@yahoo.com.br>
2023-07-21 22:19:59 -0300

    insert created and updated user date attributes

--- backend/src/main/java/br/com/gestorcultural/gestorcultural/model/entity/user/User.java
+++ backend/src/main/java/br/com/gestorcultural/gestorcultural/model/entity/user/User.java
@@ -14,51 +14,8 @@ public class User {
     private String email;
     private String password;
     private String hash;
-   private boolean isAdmin;
-   private boolean isAgree;
-   private boolean isValid;
-
- //   private List<Access> accessList; implements in the future feature
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getHash() {
-        return hash;
-    }
-
-    public void setHash(String hash) {
-        this.hash = hash;
-    }
+    private Date  createdIn;
+    private Date updateIn;
 
     public boolean getIsAdmin() {
         return isAdmin;

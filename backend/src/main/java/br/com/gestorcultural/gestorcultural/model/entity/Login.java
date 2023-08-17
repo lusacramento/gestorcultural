601130eea39d963ca265a17e9b0e17bf4f04be97
lusacramento <lucianossacramento@yahoo.com.br>
2023-08-17 13:37:21 -0300

    remove unused files

--- backend/src/main/java/br/com/gestorcultural/gestorcultural/model/entity/Login.java
+++
@@ -1,9 +0,0 @@
-package br.com.gestorcultural.gestorcultural.model.entity;
-
-import lombok.Data;
-
-@Data
-public class login {
-    private String email;
-    private String password;
-}
--- backend/src/main/java/br/com/gestorcultural/gestorcultural/service/login/LoginService.java
+++
@@ -1,2 +0,0 @@
-package br.com.gestorcultural.gestorcultural.service.login;public interface Login {
-}

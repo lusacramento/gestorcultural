5cd136eb35c9879182179a0eeedf1c1838a55d65
lusacramento <lucianossacramento@yahoo.com.br>
2023-08-23 14:17:32 -0300

    add Session Policy Stateless

--- backend/src/main/java/br/com/gestorcultural/gestorcultural/configuration/security/SecurityConfiguration.java
+++ backend/src/main/java/br/com/gestorcultural/gestorcultural/configuration/security/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -14,12 +15,14 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS ))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.GET, "/api/usuarios").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/usuarios/*").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/usuarios").permitAll()
                         .requestMatchers(HttpMethod.PUT, "/api/usuarios/*").permitAll()
                         .requestMatchers(HttpMethod.DELETE, "/api/usuarios/*").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/entrar ").permitAll()
                         .anyRequest().authenticated()
 
                 );

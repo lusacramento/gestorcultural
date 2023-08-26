63e9a7cecc820a0f2c00cfddb8d1b21193c5eb28
lusacramento <lucianossacramento@yahoo.com.br>
2023-08-26 15:52:04 -0300

    refactor hashs operations

--- backend/src/main/java/br/com/gestorcultural/gestorcultural/service/user/impl/UserServiceImpl.java
+++ backend/src/main/java/br/com/gestorcultural/gestorcultural/service/user/impl/UserServiceImpl.java
@@ -8,6 +8,7 @@ import br.com.gestorcultural.gestorcultural.service.user.UserService;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -45,6 +46,7 @@ public class UserServiceImpl implements UserService {
         if (this.userRepository.existsByLogin(user.getLogin()))
             throw new BadRequestException("Email já está cadastrado!");
 
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
         user.setCreatedIn();
         user.setUpdatedIn();
 
@@ -59,6 +61,7 @@ public class UserServiceImpl implements UserService {
         if (!this.userRepository.existsById(user.getId()))
             throw new BadRequestException("Este email não está cadastrado!");
 
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
         user.setCreatedIn(this.userRepository.findById(user.getId()).get().getCreatedIn());
         user.setUpdatedIn();
         return this.userRepository.save(user);
@@ -70,10 +73,16 @@ public class UserServiceImpl implements UserService {
         if (!this.userRepository.existsById(id)) throw new BadRequestException("Usuário não encontrado!");
 
         Optional<User> userResponse = this.userRepository.findById(id);
-        if (
-                !Objects.equals(userResponse.get().getLogin(), user.getLogin()) ||
-                        !Objects.equals(userResponse.get().getPassword(), user.getPassword())
-        ) throw new BadRequestException("Email ou senha inválida!");
+        System.out.println("Login entrada: " + user.getLogin());
+        System.out.println("Login persistido: " + userResponse.get().getLogin());
+        System.out.println("Password entrada: " + user.getPassword());
+        System.out.println("Password persistente: " + userResponse.get().getPassword());
+
+        if (!Objects.equals(userResponse.get().getLogin(), user.getLogin()))
+            throw new BadRequestException("Email ou senha inválida!");
+
+        boolean isPasswordOK = new BCryptPasswordEncoder().matches(user.getPassword(), userResponse.get().getPassword());
+        if (!isPasswordOK) throw new BadRequestException("Email ou senha inválida!");
 
         this.userRepository.deleteById(id);
     }

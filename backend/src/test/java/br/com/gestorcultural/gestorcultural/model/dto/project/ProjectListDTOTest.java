36dcec87bba3064b77de197fc6dd1addbdbfd063
lusacramento <lucianossacramento@yahoo.com.br>
2023-08-30 12:26:33 -0300

    implements project list dto

---
+++ backend/src/test/java/br/com/gestorcultural/gestorcultural/model/dto/project/ProjectListDTOTest.java
@@ -0,0 +1,40 @@
+package br.com.gestorcultural.gestorcultural.model.dto.project;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ProjectListDTOTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getOwner() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getAbstractOfProject() {
+    }
+
+    @Test
+    void setOwner() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setAbstractOfProject() {
+    }
+}
